542bf867d23b6dc91fd16620e8a0547ea5dc5965
manjunath h k <manjunathhk833@gmail.com>
2021-09-26 13:49:02 +0530

    second commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-   
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+class List extends Component{
+    render(){
+        return (
+            <ol>
+             {this.props.tasks.map((task, index) => <li key={index}>{task}</li>)}
+        </ol>)
+    }
 
+}
+
+class Title extends Component{
+    render(){
+        return <h1>{this.props.title}</h1>
+    }
+}
+
+class Main extends Component{
+    render(){
+        return ( 
+        <div> 
+            <Title title={'TO DOS'}/>
+            <List tasks = {['Do react project 2', 'Do react project 3']}/>
+            <List tasks = {['Dont do dumb stuff', 'dont mess around', 'sleep']}/>
+        </div>
+            )
+    }
+}
+ReactDOM.render(<Main/>, document.getElementById('root'));
