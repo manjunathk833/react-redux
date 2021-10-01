b96a0577e2a07521e32decd8380188371cd307ca
manjunath h k <manjunathhk833@gmail.com>
2021-10-01 19:31:22 +0530

    Photo component partial commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Main from './Components/Main.js';
-//comments
+import './styles/stylesheet.css';
+
 ReactDOM.render(<Main/>, document.getElementById('root'));
